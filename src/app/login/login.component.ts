996b115626db47a65b491885f3ddf67bf44b0483
NgocThinh <nguyen06@uvic.ca>
2018-08-06 01:01:10 -0700

    commit the login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { authenticationService } from '../services/index';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +8,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  model: any = {};
+  usernameError = false;
+  passwordError = false;
+  loading = false;
+  returnUrl: string;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private authenticationService: authenticationService
+  ) { }
 
   ngOnInit() {
+    this.authenticationService.logout();
+
+    this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
+  }
+
+  login(){
+    this.loading = true;
+    this.authenticationService.login(this.model.username, this.model.password)
+      .subscribe(
+        data => {
+          if(data.message.username === 'false' && data.message.password === 'true'){
+            this.loading = false;
+            this.usernameError = true;
+            this.passwordError = false;
+          }
+          else if(data.message.username === 'true' && data.message.password === 'false'){
+            this.loading = false;
+            this.usernameError = false;
+            this.passwordError = true;
+          }else{
+            this.router.navigate([this.returnUrl]);
+          }
+          
+        },
+        error => {
+          this.loading = false;
+        }
+      )
   }
 
 }
--- src/app/services/authentication.service.ts
+++ src/app/services/authentication.service.ts
@@ -1,83 +1,28 @@
 import { Injectable } from '@angular/core';
-import { HttpClient} from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
-import { Router } from '@angular/router';
-import { dataStructure } from './interfaceData';
-@Injectable({
-  providedIn: 'root'
-})
-export class AuthenticationService {
-  private token: string;
 
-  constructor(private http: HttpClient, private router: Router) { }
+@Injectable()
+export class authenticationService {
+  constructor(private http: HttpClient){}
 
-  private saveToken(token: string):void {
-    localStorage.setItem("mean-token", token);
-    this.token = token;
+  login(username: string, password: string){
+    return this.http.post<any>('https://pure-tor-72835.herokuapp.com/users/login', {
+      username: username,
+      password: password
+    }).pipe(map(user => {
+      if(user.status === 'false'){
+        return user;
+      }
+      else if(user && user.message.token){
+        localStorage.setItem('currentUser', JSON.stringify(user));
+      }
+      return user;
+      
+    }));
   }
-  private getToken(): string{
-    if(!this.token){
-      this.token = localStorage.getItem('mean-token');
-    }
-    return this.token;
+  logout(){
+    localStorage.removeItem('currentUser');
   }
-  private getUserDetails(): dataStructure.UserDetails{
-    const token = this.getToken();
-    let payload;
-    if(token){
-      payload = token.split('.')[1];
-      payload = window.atob(payload);
-      return JSON.parse(payload);
-    }else{
-      return null;
-    }
-  }
-
-  public isLoggedIn(): boolean {
-    const user = this.getUserDetails();
-    if(user){
-      return user.exp > Date.now()/1000;
-    }else{
-      return false;
-    }
-  }
-
-  private request(method: 'post'|'get', 
-                  type: 'login'|'register'|'profile',
-                  user?: dataStructure.TokenPayload): Observable<any>
-  {
-    let base;
-    if(method === 'post'){
-      base = this.http.post(`/api/${type}`, user);
-    }else{
-      base = this.http.get(`/api/${type}`, {headers: { authorization: `Bearer ${this.getToken()}`}});
-    }
-    const request = base.pipe(
-      map((data: dataStructure.tokenResponse) => {
-        if(data.token){
-          this.saveToken(data.token);
-        }
-        return data;
-      })
-    );
-    return request;
-  }
-
-  public register(user: dataStructure.TokenPayload): Observable<any> {
-    return this.request('post', 'register', user);
-  }
-  public login(user: dataStructure.TokenPayload): Observable<any>{
-    return this.request('post','login', user);
-  }
-  public profile(): Observable<any>{
-    return this.request('get', 'profile');
-  }
-
-  public logout(): void {
-    this.token = '';
-    window.localStorage.removeItem("mean-token");
-    this.router.navigateByUrl('/');
-  }
-
 }
